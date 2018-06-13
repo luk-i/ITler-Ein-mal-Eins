4488c7950c21ba0d55f8ce310963fad50d643571
litlepoop <simon.popp@live.de>
2018-06-13 08:55:43 +0200

    Fenster für Rechner erstellt

---
+++ Client/UnitCalculator/UnitCalculator.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITler_Ein_mal_Eins.UnitCalculator
+{
+    /// <summary>
+    /// Interaktionslogik für UnitCalculator.xaml
+    /// </summary>
+    public partial class UnitCalculator : Window
+    {
+        public UnitCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
