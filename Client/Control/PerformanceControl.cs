9c418b64d0bfe5fd4f896fb419176eeb9fb192f7
litlepoop <simon.popp@live.de>
2018-06-27 20:22:25 +0200

    Neue Controlklasse für performance

---
+++ Client/Control/PerformanceControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITler_Ein_mal_Eins.Control
+{
+    class PerformanceControl
+    {
+
+    }
+}
